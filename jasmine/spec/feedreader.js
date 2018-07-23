d46d6a3c19aa72ff573ee9b1556fb6a66cae021a
Elena Moral <elenamoralpascual@gmail.com>
2018-07-23 10:50:21 +0200

    added loops for names & url

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it("have URLs", function() {
+            allFeeds.forEach(function(eachFeed) {
+              expect(eachFeed.url).toBeDefined();
+              expect(eachFeed.url.length).not.toBe(0);
+            });
+          });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it("have names", function() {
+            allFeeds.forEach(function(eachFeed) {
+              expect(eachFeed.name).toBeDefined();
+              expect(eachFeed.name.length).not.toBe(0);
+            });
+          });
     });
 
 
